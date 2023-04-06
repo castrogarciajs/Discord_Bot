d605f5c425a01f99dc0ae054d76d13d54700dbc3
Sebastian009w <ia3266595@gmail.com>
2023-04-05 22:17:40 -0500

    se muestra estadisitcas

--- src/index.py
+++ src/index.py
@@ -1,5 +1,6 @@
 import discord
 import os
+import datetime
 from dotenv import load_dotenv
 from discord.ext import commands
 
@@ -21,11 +22,27 @@ async def init(ctx):
 async def sum(ctx, number_one: int, number_two: int):
     await ctx.send(number_one + number_two)
 
+@bot.command()
+async def stats(ctx):
+    embed = discord.Embed(
+        title='Gy', 
+        description='7cero3 Bot', 
+        timestamp=datetime.datetime.utcnow(), 
+        color=discord.Color.blue()
+    )
+
+    embed.add_field(name='Server Created At', value=f"{ctx.guild.created_at}")
+    embed.add_field(name='Server Owner', value=f"{ctx.guild.owner}")
+    embed.add_field(name='Server ID', value=f"{ctx.guild.id}")
+    embed.set_image(url=f"{ctx.guild.icon}")
+    
+    await ctx.send(embed=embed)
+
 # Event
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(activity=discord.Streaming(name='Example', url='http://www.twitch.tv/accountname'))
+    await bot.change_presence(activity=discord.Streaming(name='Mousak', url='http://www.twitch.tv/mousak'))
     print('My Bot On ready')
 
 try:
